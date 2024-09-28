d9e1ba7f08e7f9aab9900b56a6b2ace17507c734
Alexandr-Fox <al.rostilismith@yandex.ru>
2024-09-28 22:05:53 +0500

    Add http requests berloga class

---
+++ src/APItalent/Http.cs
@@ -0,0 +1,80 @@
+﻿namespace APItalent;
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class Http
+{
+    private static readonly HttpClient HttpClient = new();
+    private static readonly string? BaseUrl = Environment.GetEnvironmentVariable("BASE_ADRESS");
+
+    public static async Task<T?> Post<T>(string path, object? data = null, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        var requestBody = data != null ? new StringContent(JsonSerializer.Serialize(data),
+            Encoding.UTF8,
+            "application/json") : null;
+        var response = await HttpClient.PostAsync($"{BaseUrl}/{path}", requestBody);
+
+        var jsonResponce = await response.Content.ReadAsStringAsync();
+
+        if (response.StatusCode == statusCode)
+        {
+            return JsonSerializer.Deserialize<T>(jsonResponce);
+        }
+
+        var err = JsonSerializer.Deserialize<HttpErrorResponse>(jsonResponce);
+        throw new HttpError(statusCode, err);
+    }
+}
+
+public class HttpError : Exception
+{
+    /// <summary>
+    ///   Error description from server answer
+    /// </summary>
+    public string ErrorMessage { get; set; }
+    public string? VerboseMessage { get; set; }
+    /// <summary>
+    ///   Error code
+    /// </summary>
+    public HttpStatusCode StatusCode { get; set; }
+
+    /// <summary>
+    /// <param name="statusCode">Http error code.</param>
+    /// <param name="errorResponse">Error description from server answer.</param>
+    /// </summary>
+    /// <summary>
+    /// Berloga API Error class.
+    /// </summary>
+    /// <returns>A <typeparamref name="HttpError"/> representation of the Berloga API error.</returns>
+    /// <param name="statusCode">Http error code.</param>
+    /// <param name="errorResponse">Error description from server answer.</param>
+    public HttpError(HttpStatusCode statusCode, HttpErrorResponse? errorResponse)
+        : base(errorResponse != null ?
+            errorResponse.verbose_message != null
+                ? errorResponse.verbose_message : errorResponse.error_message
+            : "Не возможно десеализировать ответ")
+    {
+        StatusCode = statusCode;
+
+        if (errorResponse != null)
+        {
+            ErrorMessage = errorResponse.error_message;
+            VerboseMessage = errorResponse.verbose_message;
+        }
+        else
+        {
+            ErrorMessage = "Не возможно десеализировать ответ";
+        }
+    }
+}
+
+public class HttpErrorResponse
+{
+    public string error_message { get; set; }
+    public string? verbose_message { get; set; } = null;
+}
